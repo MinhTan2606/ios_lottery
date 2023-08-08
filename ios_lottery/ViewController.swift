402ae48fb7ac1d487098229c7d2366c811ce5601
Tan Phan-Minh <minhtanphan2696@gmail.com>
2023-08-08 20:32:55 +0700

    Init new ios source

---
+++ ios_lottery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_lottery
+//
+//  Created by Bean on 08/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
